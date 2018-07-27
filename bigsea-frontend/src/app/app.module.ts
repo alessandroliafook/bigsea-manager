e68919d1f3e6dd6b0948ddf2245d4e36fa4aafc1
Matheus P. Silva <matheus.ps@icloud.com>
2018-07-26 21:48:25 -0300

    added submission redirect

--- bigsea-frontend/src/app/app.module.ts
+++ bigsea-frontend/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { routing } from './config/routes.config';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SubmissionsComponent } from './components/submissions/submissions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NotFoundComponent,
+    SubmissionsComponent,
   ],
   imports: [
     BrowserModule,
--- bigsea-frontend/src/app/config/routes.config.ts
+++ bigsea-frontend/src/app/config/routes.config.ts
@@ -1,10 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from  '../components/login/login.component';
 import { NotFoundComponent } from '../components/not-found/not-found.component';
+import { SubmissionsComponent } from '../components/submissions/submissions.component';
 
 
 const appRoutes: Routes = [
     { path: '', component: LoginComponent},
+    { path: 'submissions', component: SubmissionsComponent},
     { path: '**', component: NotFoundComponent}
   ];
 
